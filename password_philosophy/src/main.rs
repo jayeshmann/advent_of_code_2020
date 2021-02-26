6e65c98fae32f2e12b538af436f190081f4ca2de
Jayesh Mann <jayeshmann06@gmail.com>
2021-02-26 21:12:38 +0530

    Password Philosophy Part2 solution

--- password_philosophy/src/main.rs
+++ password_philosophy/src/main.rs
@@ -1,8 +1,7 @@
 use std::{fs, process};
 
 fn main() {
-    let file_name = "b";
-    let file = fs::read_to_string(format!("{}.in", file_name)).unwrap_or_else(|err| {
+    let file = fs::read_to_string("b.in").unwrap_or_else(|err| {
         eprintln!("Problem reading file: {}", err);
         process::exit(1);
     });
@@ -14,17 +13,18 @@ fn main() {
         let line_split: Vec<&str> = line.split(|s| s == '-' || s == ' ' || s == ':').collect();
         // println!("{:?}", line_split);
 
-        let lower: u32 = line_split[0].parse().unwrap();
-        let upper: u32 = line_split[1].parse().unwrap();
+        let lower: usize = line_split[0].parse().unwrap();
+        let upper: usize = line_split[1].parse().unwrap();
         let char_check: char = line_split[2].parse().unwrap();
         let password = line_split[4];
-        let mut count = 0;
-        for char in password.chars() {
-            if char == char_check {
-                count += 1;
-            }
+
+        let first_char = password.chars().nth(lower - 1).unwrap();
+        let second_char = password.chars().nth(upper - lower).unwrap();
+        // println!("first: {}, second: {}", first_char, second_char);
+        if first_char == second_char {
+            continue;
         }
-        if count >= lower && count <= upper {
+        if char_check == first_char || char_check == second_char {
             pass_count += 1;
         }
     }
