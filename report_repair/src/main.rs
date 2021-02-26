c69a15bc7deedbd7e981767f5bdfabc16e570f15
Jayesh Mann <jayeshmann06@gmail.com>
2021-02-26 14:03:59 +0530

    Report Repair

---
+++ report_repair/src/main.rs
@@ -0,0 +1,25 @@
+use std::{fs, process};
+
+fn main() {
+    let file_name = "b";
+    let file = fs::read_to_string(format!("{}.in", file_name)).unwrap_or_else(|err| {
+        eprintln!("Problem reading file: {}", err);
+        process::exit(1);
+    });
+
+    let contents = file.lines();
+    let values: Vec<u32> = contents.map(|line| line.parse().unwrap()).collect();
+
+    for i in &values {
+        for j in &values {
+            if (i + j) == 2020 {
+                fs::write(format!("{}.out", file_name), format!("{}", i * j)).unwrap_or_else(
+                    |err| {
+                        eprintln!("Problem writing file: {}", err);
+                        process::exit(1);
+                    },
+                );
+            }
+        }
+    }
+}
